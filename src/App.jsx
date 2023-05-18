d9380990f2876d010ccc729537533e5c2cef63e7
najmul905 <nh4808563@gmail.com>
2023-05-18 17:50:41 +0600

    create a navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+import './App.css'
+import Home from './Components/Home/Home/Home'
+
+function App() {
+
+
+  return (
+    < div className=' bg-orange-200'>
+      <Home></Home>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Home/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import HeaderLayOut from '../HeaderLayOut/HeaderLayOut';
+import Footer from '../Footer/Footer';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div className='md:mx-12 mx-4'>
+            <HeaderLayOut></HeaderLayOut>
+            <Banner></Banner>
+            <h1>This is Home page</h1>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
